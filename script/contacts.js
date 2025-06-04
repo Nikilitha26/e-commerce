708058e92392621d49b4b180504a1b76f0ce548f
Nikilitha <kakazanikilitha544@gmail.com>
2025-06-04 11:19:50 +0200

    fixing contact page

--- script/contacts.js
+++ script/contacts.js
@@ -1,3 +1,38 @@
+// nav
+const current = window.location.href;
+document.querySelectorAll('.nav-link').forEach(link => {
+  if (link.href === current) {
+    link.classList.add('active');
+  }
+});
+
+document.querySelectorAll('.category-slideshow').forEach(slideshow => {
+  const slides = slideshow.querySelectorAll('.slide');
+  let index = 0;
+  slides[index].classList.add('active');
+
+  setInterval(() => {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+  }, 3000); // Change image every 3 seconds
+});
+
+
+function updateCartCount() {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  let totalCount = 0;
+  cart.forEach(item => {
+    totalCount += item.quantity;
+  });
+
+  const cartCountElement = document.getElementById('cart-count');
+  if (cartCountElement) {
+    cartCountElement.textContent = totalCount;
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('form');
 
